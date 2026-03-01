91c1837c9c064a0fd338c539e1da9da3773391a6
SindreSau <sindre_50@hotmail.com>
2026-03-01 14:25:49 +0100

    commit before checking solution

---
+++ 6-State/add-movie-statistics-1/src/data/movies.js
@@ -0,0 +1,64 @@
+export const ALL_MOVIES = {
+    items: [
+        {
+            id: 1,
+            name: 'The Godfather',
+            description:
+                'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+            image: 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UY1982_.jpg',
+            rating: '4',
+            genres: ['Crime', 'Drama'],
+            inTheaters: false,
+        },
+        {
+            id: 2,
+            name: 'The Shawshank Redemption',
+            description:
+                'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+            image: 'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1200_.jpg',
+            rating: 4,
+            genres: ['Drama'],
+            inTheaters: false,
+        },
+        {
+            id: 3,
+            name: 'The Dark Knight',
+            description:
+                'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+            image: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UY2048_.jpg',
+            rating: 3,
+            genres: ['Action', 'Crime', 'Drama'],
+            inTheaters: true,
+        },
+        {
+            id: 4,
+            name: 'Inception',
+            description:
+                'A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
+            image: 'https://cdn11.bigcommerce.com/s-yzgoj/images/stencil/1280x1280/products/2919271/5944675/MOVEB46211__19379.1679590452.jpg?c=2',
+            rating: 4,
+            genres: ['Action', 'Sci-Fi', 'Thriller'],
+            inTheaters: false,
+        },
+        {
+            id: 5,
+            name: 'Pulp Fiction',
+            description:
+                'The lives of two mob hitmen, a boxer, a gangster and his wife intertwine in four tales of violence and redemption.',
+            image: 'https://static.posters.cz/image/1300/1288.jpg',
+            rating: 4,
+            genres: ['Crime', 'Drama'],
+            inTheaters: false,
+        },
+        {
+            id: 6,
+            name: 'Forrest Gump',
+            description:
+                'The presidencies of Kennedy and Johnson unfold from the perspective of an Alabama man with an IQ of 75.',
+            image: 'https://m.media-amazon.com/images/M/MV5BNDYwNzVjMTItZmU5YS00YjQ5LTljYjgtMjY2NDVmYWMyNWFmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+            rating: 4,
+            genres: ['Drama', 'Romance'],
+            inTheaters: false,
+        },
+    ],
+};
